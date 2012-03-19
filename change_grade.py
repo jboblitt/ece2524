5b55757631e27276664cf04c2001e81d0c8a444d
Justin Boblitt <jboblitt@vt.edu>
2012-03-19 17:32:47 -0400

    Made script into main and function

--- change_grade.py
+++ change_grade.py
@@ -1,30 +1,32 @@
 #!/usr/bin/env python
-
-from sys import stdin,stdout,stderr, argv, exit
 import re
-import os.path
+def change_grade(stdin, nameToFind, gradeChange):
+      for availLine in stdin:
+            if re.search(name, availLine):
+                  stdout.write(re.sub("[0-9].", grade, availLine))
+            else:
+                  stdout.write(availLine)
 
-scriptName=argv[0]
-name=argv[1]
-grade=argv[2]
+if __name__ == '__main__':
+      from sys import stdin,stdout,stderr, argv, exit
+      import os.path
+      if len(argv) > 3:    
+            scriptName=argv[0]
+            name=argv[1]
+            grade=argv[2]
+            fileName=argv[3]
+            #if exists.      
+            if os.path.exists(fileName):
+                  #Print reading lines from file.  Switch file to over to stdin
+                  stdin=file(fileName)
+            else:
+                  #Print error and exit.
+                  stderr.write(scriptName + ": " + fileName + ": no such file or directory \n")
+                  exit(1)
+      else: 
+            stderr.write("Change arguments to <name of kid> <grade %> <file report card>")
+      change_grade(stdin, name, grade)
+      exit(0)    
 
-#Check if received atleast one argument.
-if len(argv) > 3:
-      fileName=argv[3]
-      #if exists.      
-      if os.path.exists(fileName):
-            #Print reading lines from file.  Switch file to over to stdin
-            stdin=file(fileName)
-      else:
-            #Print error and exit.
-            stderr.write(scriptName + ": " + fileName + ": no such file or directory \n")
-            exit(1)      
 
-linecount=0;
-#print line by line.
-for availLine in stdin:
-      if re.search(name, availLine):
-            stdout.write(re.sub("[0-9].", grade, availLine))
-      else:
-            stdout.write(availLine)
-exit(0)
+
